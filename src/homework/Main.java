fa7d04ee2d3bde90a2cce1e85e3069e89754b60e
TDumler74 <tatjana.dumler@gmail.com>
2023-03-15 19:33:36 +0100

    sec

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -1,5 +1,9 @@
 package homework;
-
+/*
+Создать класс MyDate с полями day,month,year, конструктором,геттерами и сеттерами.
+Создать несколько одинаковых обьектов этого класса и проверить их на равенство.
+Затем переопределить методы equals и hashCode и вновь проверить обьекты на равенство.
+ */
 public class Main {
     public static void main(String[] args) {
         MyDate myDate1= new MyDate(02,"janury",2023);
